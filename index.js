7f868824adca6c1f1737798d08977594bdb33748
prxshetty <prxshetty@gmail.com>
2024-07-03 03:16:15 -0400

    temp 1 chatbot

---
+++ index.js
@@ -0,0 +1,35 @@
+import fs from 'fs/promises';
+import { RecursiveCharacterTextSplitter} from 'langchain/text_splitter'
+import {createClient} from '@supabase/supabase-js'
+import {SupabaseVectorStore} from 'langchain/vectorstores/supabase'
+import {OpenAIEmbeddings} from 'langchain/embeddings/openai'
+
+try{
+    const text=await fs.readFile('scrimba-info.txt','utf-8')
+    const splitter=new RecursiveCharacterTextSplitter({
+        chunkSize: 500,
+        separators: ['/n/n','/n',' ',''],
+        chunkOverlap:50
+    })
+    const output=await splitter.createDocuments([text])
+    
+    const sbApiKey=process.env.SUPABASE_API_KEY
+    const sbUrl=process.env.SUPABASE_URL_LC_CHATBOT
+    const openAIApiKey= process.env.OPENAI_API_KEY
+    
+    const cleint = createClient(sbUrl, sbApiKey)
+
+
+    await SupabaseVectorStore.fromDocuments(
+        output,
+        new OpenAIEmbeddings({openAIApiKey}),
+        {
+            client,
+            tablename: 'documents',
+        }
+    )
+    
+    console.log(output)
+} catch(err){
+    console.log(err)
+}
