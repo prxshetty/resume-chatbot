13d6dceb3b036a565d1eab6767ad1f03fabbefc4
prxshetty <prxshetty@gmail.com>
2024-07-09 19:31:23 -0400

    piping retriver, vbase, refactor, added StringOutputParser

--- index.js
+++ index.js
@@ -1,36 +1,67 @@
-import fs from 'fs/promises';
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
-import { createClient } from '@supabase/supabase-js';
-import { SupabaseVectorStore } from 'langchain/vectorstores/supabase';
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { ChatOpenAI } from "langchain/chat_models/openai"
+import { PromptTemplate } from "langchain/prompts"
+
+import { SupabaseVectorStore } from "langchain/vectorstores/supabase"
+import { OpenAIEmbeddings } from "langchain/embeddings/openai"
+import { createClient } from "@supabase/supabase-js"
+import {StringOutputParser} from 'langchain/schema/output_parser'
 import dotenv from 'dotenv'
 dotenv.config()
-try{
-    const text=await fs.readFile('scrimba-info.txt','utf-8')
-    const splitter=new RecursiveCharacterTextSplitter({
-        chunkSize: 500,
-        separators: ['/n/n','/n',' ',''],
-        chunkOverlap:50
-    })
-    const output=await splitter.createDocuments([text])
-    
-    const sbApiKey=process.env.SUPABASE_API_KEY
-    const sbUrl=process.env.SUPABASE_URL_LC_CHATBOT
-    const openAIApiKey= process.env.OPENAI_API_KEY
-    
-    const client = createClient(sbUrl, sbApiKey)
+
+// document.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     progressConversation()
+// })
+
+const openAIApiKey = process.env.OPENAI_API_KEY
+
+const embeddings = new OpenAIEmbeddings({ openAIApiKey })
+const sbApiKey = process.env.SUPABASE_API_KEY
+const sbUrl = process.env.SUPABASE_URL_LC_CHATBOT
+const client = createClient(sbUrl, sbApiKey)
+
+const vectorStore=new SupabaseVectorStore(embeddings, {
+    client,
+    tableName: 'documents',
+    queryName: 'match_documents'
+});
+
+const retriever=vectorStore.asRetriever();
 
 
-    await SupabaseVectorStore.fromDocuments(
-        output,
-        new OpenAIEmbeddings({openAIApiKey}),
-        {
-            client,
-            tablename: 'documents',
-        }
-    )
-    
-    // console.log(output)
-} catch(err){
-    console.log(err)
+const llm = new ChatOpenAI({ openAIApiKey })
+
+const standaloneQuestionTemplate = 'Given a question, convert it to a standalone question. question: {question} standalone question:'
+
+const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate);
+
+const Chain = standaloneQuestionPrompt.pipe(llm).pipe(new StringOutputParser()).pipe(retriever);
+
+// const response2= await retriever.invoke('Will Scrimba work on an old laptop?')
+
+const response = await Chain.invoke({
+    question: 'What are the technical requirements for running Scrimba? I only have a very old laptop which is not that powerful.'
+});
+
+console.log(response)
+
+async function progressConversation() {
+    const userInput = document.getElementById('user-input')
+    const chatbotConversation = document.getElementById('chatbot-conversation-container')
+    const question = userInput.value
+    userInput.value = ''
+
+    // add human message
+    const newHumanSpeechBubble = document.createElement('div')
+    newHumanSpeechBubble.classList.add('speech', 'speech-human')
+    chatbotConversation.appendChild(newHumanSpeechBubble)
+    newHumanSpeechBubble.textContent = question
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+
+    // add AI message
+    const newAiSpeechBubble = document.createElement('div')
+    newAiSpeechBubble.classList.add('speech', 'speech-ai')
+    chatbotConversation.appendChild(newAiSpeechBubble)
+    newAiSpeechBubble.textContent = result
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
 }
