5c7be30c2ee84c285b7f328f17fac6f4ff4cb204
prxshetty <prxshetty@gmail.com>
2024-07-13 21:38:09 -0400

    organized, version updates

--- index.js
+++ index.js
@@ -1,47 +1,52 @@
-import { ChatOpenAI } from "langchain/chat_models/openai"
-import { PromptTemplate } from "langchain/prompts"
+import { ChatOpenAI } from 'langchain/chat_models/openai'
+import { PromptTemplate } from 'langchain/prompts'
+import { StringOutputParser } from 'langchain/schema/output_parser'
+import { retriever } from '/utils/retriever'
 
-import { SupabaseVectorStore } from "langchain/vectorstores/supabase"
-import { OpenAIEmbeddings } from "langchain/embeddings/openai"
-import { createClient } from "@supabase/supabase-js"
-import {StringOutputParser} from 'langchain/schema/output_parser'
-import dotenv from 'dotenv'
-dotenv.config()
-
-// document.addEventListener('submit', (e) => {
-//     e.preventDefault()
-//     progressConversation()
-// })
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    progressConversation()
+}) 
 
 const openAIApiKey = process.env.OPENAI_API_KEY
-
-const embeddings = new OpenAIEmbeddings({ openAIApiKey })
-const sbApiKey = process.env.SUPABASE_API_KEY
-const sbUrl = process.env.SUPABASE_URL_LC_CHATBOT
-const client = createClient(sbUrl, sbApiKey)
-
-const vectorStore=new SupabaseVectorStore(embeddings, {
-    client,
-    tableName: 'documents',
-    queryName: 'match_documents'
-});
-
-const retriever=vectorStore.asRetriever();
-
-
 const llm = new ChatOpenAI({ openAIApiKey })
 
 const standaloneQuestionTemplate = 'Given a question, convert it to a standalone question. question: {question} standalone question:'
 
-const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate);
+const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate)
 
-const Chain = standaloneQuestionPrompt.pipe(llm).pipe(new StringOutputParser()).pipe(retriever);
 
-// const response2= await retriever.invoke('Will Scrimba work on an old laptop?')
+const answerTemplate = `You are a helpful and ethusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context.
+ If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make
+ up and answer. Always speak as if you were chatting to a friend.
+ context: {context}
+ question: {question}
+ answer:
+ `
 
-const response = await Chain.invoke({
+const answerPrompt= PromptTemplate.fromTemplate(answerTemplate);
+/**
+ * Challenge:
+ * 1. Create a template and prompt to get an answer to 
+ *    the user's original question. Remember to include 
+ *    the original question and the text chunks we got 
+ *    back from the vector store as input variables. Call 
+ *    these input variables 'original_question' and 'context'.
+ * ⚠️ Feel free to add this to the chain, but you will get 
+ *    an error.
+ * 
+ * We want this chatbot to:
+ *  - be friendly
+ *  - only answer from the context provided and never make up 
+ *    answers
+ *  - apologise if it doesn't know the answer and advise the 
+ *    user to email help@scrimba.com
+*/
+const chain = standaloneQuestionPrompt.pipe(llm).pipe(new StringOutputParser()).pipe(retriever).pipe(answerPrompt)
+
+const response = await chain.invoke({
     question: 'What are the technical requirements for running Scrimba? I only have a very old laptop which is not that powerful.'
-});
+})
 
 console.log(response)
 
