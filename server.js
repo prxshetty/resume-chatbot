081885e27ae215e7c07174394acd5173934a4fd4
prxshetty <prxshetty@gmail.com>
2024-07-15 15:12:02 -0400

    text embed for resume added, path fix

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ question: {question}
 standalone question:`
 const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate)
 
-const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about the Resume based on the context provided
+const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about the Resume of a student named "Pranam" based on the context provided
  and the conversation history. Try to find the answer in the context. If the answer is not given in the context, 
  find the answer in the conversation history if possible. 
  If you really don't know the answer, say "I'm sorry, I don't know the answer to that." 
---
+++ txt_embed.js
@@ -0,0 +1,48 @@
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
+import * as dotenv from 'dotenv'
+import path from 'path'
+import { createClient } from '@supabase/supabase-js'
+import { SupabaseVectorStore } from 'langchain/vectorstores/supabase'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+import { fileURLToPath } from 'url'
+dotenv.config()
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const pdfPath = path.join(__dirname, 'PranamShettyF_Resume.pdf')
+// console.log('Current directory:', __dirname)
+// console.log('Resolved PDF path:', pdfPath)
+async function main() {
+    try {
+        const loader = new PDFLoader(pdfPath)
+        const docs = await loader.load()
+        // console.log('Extracted PDF Documents:', docs)
+
+        const splitter = new RecursiveCharacterTextSplitter({
+            chunkSize: 200,
+            chunkOverlap: 25,
+            separators: ['\n\n', '\n', ' ', '']
+        })
+        const splitDocs = await splitter.splitDocuments(docs)
+        // console.log('Split Documents:', splitDocs)
+        const sbApiKey = process.env.SUPABASE_API_KEY
+        const sbUrl = process.env.SUPABASE_URL_LC_CHATBOT
+        const openAIApiKey = process.env.OPENAI_API_KEY
+        const client = createClient(sbUrl, sbApiKey)
+
+        await SupabaseVectorStore.fromDocuments(
+            splitDocs,
+            new OpenAIEmbeddings({ openAIApiKey }),
+            {
+                client,
+                tableName: 'documents',
+            }
+        )
+
+        console.log('Documents have been successfully added to the vector store.')
+    } catch (err) {
+        console.log('Error:', err)
+    }
+}
+main()
