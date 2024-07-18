b093019b1d3c139228dab41b651233048818b6a6
prxshetty <prxshetty@gmail.com>
2024-07-18 00:51:24 -0400

    prompt updates, send img changed to match the theme

--- server.js
+++ server.js
@@ -22,12 +22,13 @@ question: {question}
 standalone question:`;
 const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate);
 
-const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about the Resume of a student named "Pranam" based on the context provided
- and the conversation history. Try to find the answer in the context. If the answer is not given in the context, 
- find the answer in the conversation history if possible. 
- If you really don't know the answer, say "I'm sorry, I don't know the answer to that." 
- And direct the questioner to email prxshetty@gmail.com or DM Pranam. Don't try to make up an answer.
- Always speak as if you were chatting to a friend.
+const answerTemplate = `You are a highly knowledgeable and enthusiastic support bot, dedicated to assisting with questions about the Resume of a student named 'Pranam' based on the provided context and the conversation history. 
+    Your responses should be precise, friendly, and professional, reflecting a genuine willingness to help. 
+    - When the answer is in the context, provide a clear and concise response. 
+    - If the answer is not in the context, search through the conversation history for relevant information. 
+    - If the answer is still not found, respond with, 'I'm sorry, I don't know the answer to that.
+    - Kindly direct the questioner in a friendly, funny and a creative way to email prxshetty@gmail.com for further assistance. 
+    Remember, do not fabricate answers. Always communicate as if you were chatting with a friend, maintaining a warm and approachable tone. 
 context: {context}
 conversation history: {conv_history}
 question: {question}
